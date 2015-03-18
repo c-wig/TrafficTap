343b701ec92047a900b78e0ba896c01babcb246c
c-wig <colin.ludwig@gmail.com>
2015-03-17 20:11:07 -0400

    Update 1
    Added new class, that runs after oneway road is found (eventually to
    have spawns added)

--- Class1.cs
+++ Class1.cs
@@ -21,18 +21,36 @@ namespace TrafficTap
         }
 
         public override void OnLevelLoaded(LoadMode mode){
-            NetCollection component = null;
+            NetCollection allRoads = null;
+            NetInfo oldRoad = null;
             if (mode != LoadMode.NewAsset && mode != LoadMode.LoadAsset)
                 return;
-            if(mode == LoadMode.NewAsset || mode == LoadMode.LoadAsset)
-                DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "Test (New Asset)");
-                component = GameObject.Find("Road").GetComponent<NetCollection>();
-                if (component != null)
-                    DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, component.name);
-            //NetTool.CreateNode(...); //Creates a road node
-            NetCollection roads = GameObject.Find("Road").GetComponent<NetCollection>();
-            DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "Test (On Level Loaded");
+
+            if(mode == LoadMode.NewAsset || mode == LoadMode.LoadAsset) //If we are in the asset creator
+                try
+                {
+                    DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "Test (New Asset)");
+                    allRoads = GameObject.Find("Road").GetComponent<NetCollection>();
+                    if (allRoads != null)
+                        foreach (NetInfo i in allRoads.m_prefabs)
+                            if (i.name == "Oneway Road")
+                                oldRoad = i;
+                                addRoad(oldRoad, allRoads);
+                    DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, oldRoad.name);
+                }
+                finally { 
+                    DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "Finally code running");
+
+                 }
         }
+
+        public void addRoad(NetInfo oldRoad, NetCollection roadList){
+            GameObject newRoad = UnityEngine.GameObject.Instantiate<GameObject>(oldRoad.gameObject);
+            newRoad.name = "Spawn Road";
+            DebugOutputPanel.AddMessage(ColossalFramework.Plugins.PluginManager.MessageType.Message, "Ran new Class");
+            //newRoad.AddComponent() Find out spawn type to put in here!
+        }
+      
     }
 
 }
